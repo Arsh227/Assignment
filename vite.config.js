00a5075910240cc1b39b749cf19a3b302ca2f626
Arsh Khandpur <arshkhandpur@gmail.com>
2025-12-02 09:02:41 -0500

    Configure Vite for MediaPipe and improve Hands import validation
    - Add optimizeDeps exclude for MediaPipe modules
    - Add commonjsOptions for MediaPipe in build config
    - Add better type checking before creating Hands instance
    - Add detailed logging to debug constructor issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@mediapipe/hands', '@mediapipe/camera_utils'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@mediapipe/, /node_modules/],
+    },
+  },
 })
 
